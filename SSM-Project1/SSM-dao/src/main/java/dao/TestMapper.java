9dcfc0c385c780cd25ac84017812908f7f631a3e
niuhuojian <71054646+niuhuojian@users.noreply.github.com>
2022-02-28 22:44:21 +0800

    Delete SSM-Project1 directory

--- SSM-Project1/SSM-dao/src/main/java/dao/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package dao;
-
-import domain.Test;
-
-import java.util.List;
-
-public interface TestMapper {
-    public List<Test> findAllTest();
-}
--- SSM-Project1/SSM-service/src/main/java/service/TestService.java
+++
@@ -1,9 +0,0 @@
-package service;
-
-import domain.Test;
-
-import java.util.List;
-
-public interface TestService {
-    public List<Test> findAllTest();
-}
