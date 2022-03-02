7ed9844c999ff642974c106378438efe49f74732
jake <1727546144@qq.com>
2022-03-02 12:43:19 +0800

    增加AOP，打印耗时 去掉filter和interceptor

--- wiki/src/main/java/demo/config/SpringMVCConfig.java
+++ wiki/src/main/java/demo/config/SpringMVCConfig.java
@@ -1,22 +1,22 @@
-package demo.config;
-
-import demo.interceptor.LogInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-import javax.annotation.Resources;
-
-@Configuration
-public class SpringMVCConfig implements WebMvcConfigurer {
-    @Resource
-    LogInterceptor logInterceptor;
-
-
-    public void addInterceptors(InterceptorRegistry registry){
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package demo.config;
+//
+//import demo.interceptor.LogInterceptor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//import javax.annotation.Resources;
+//
+//@Configuration
+//public class SpringMVCConfig implements WebMvcConfigurer {
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//
+//    public void addInterceptors(InterceptorRegistry registry){
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
--- wiki/src/main/java/demo/interceptor/LogInterceptor.java
+++ wiki/src/main/java/demo/interceptor/LogInterceptor.java
@@ -1,31 +1,31 @@
-package demo.interceptor;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-@Component
-public class LogInterceptor implements HandlerInterceptor {
-    private static final Logger Log= LoggerFactory.getLogger(LogInterceptor.class);
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        Log.info("--------LogInterceptor开始--------");
-        Log.info("请求地址：{} {}",request.getRequestURL().toString(),request.getMethod());
-        Log.info("远程地址：{}",request.getRemoteAddr());
-
-        long startTime=System.currentTimeMillis();
-        request.setAttribute("requestStartTime",startTime);
-        return true;
-    }
-
-    @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        long startTime=(Long) request.getAttribute("requestStartTime");
-        Log.info("----------LogInterceptor结束 耗时：{}ms-------",System.currentTimeMillis()-startTime);
-    }
-}
+//package demo.interceptor;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.HandlerInterceptor;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//@Component
+//public class LogInterceptor implements HandlerInterceptor {
+//    private static final Logger Log= LoggerFactory.getLogger(LogInterceptor.class);
+//
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        Log.info("--------LogInterceptor开始--------");
+//        Log.info("请求地址：{} {}",request.getRequestURL().toString(),request.getMethod());
+//        Log.info("远程地址：{}",request.getRemoteAddr());
+//
+//        long startTime=System.currentTimeMillis();
+//        request.setAttribute("requestStartTime",startTime);
+//        return true;
+//    }
+//
+//    @Override
+//    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//        long startTime=(Long) request.getAttribute("requestStartTime");
+//        Log.info("----------LogInterceptor结束 耗时：{}ms-------",System.currentTimeMillis()-startTime);
+//    }
+//}
