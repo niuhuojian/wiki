49b6f61d7c1fff4574cfed50db5abbf1807fe2a5
jake <1727546144@qq.com>
2022-03-07 11:58:50 +0800

    电子书快照增加唯一键，一个电子书一天只能有一条记录

--- wiki/doc/all.sql
+++ wiki/doc/all.sql
@@ -123,6 +123,30 @@ create table ebook_snapshot
     vote_count    int                   not null default 0 comment '点赞数',
     view_increase int                   not null default 0 comment '阅读增长',
     vote_increase int                   not null default 0 comment '点赞增长',
-    primary key (id)
+    primary key (id),
+    unique key ebook_id_date_unique(ebook_id,date)
 ) engine=innodb default charset=utf8mb4 comment='电子书快照表';
 
+#创建今日数据表，如果今日还没有数据的话
+insert into ebook_snapshot(ebook_id, date, view_count, vote_count, view_increase, vote_increase)
+select t1.id, curdate(),0,0,0,0
+from ebook t1
+where not exists(select 1 from ebook_snapshot t2 where t1.id=t2.ebook_id and t2.date=curdate());
+
+#更新今日数据
+update ebook_snapshot t1,ebook t2
+set t1.view_count=t2.view_count,t1.vote_count=t2.vote_count
+where t1.date=curdate()
+and t1.ebook_id=t2.id;
+
+#获取昨天数据
+select t1.ebook_id,view_count,vote_count from ebook_snapshot t1
+where t1.date=date_sub(curdate(),interval 1 day );
+
+#更新增量
+update ebook_snapshot t1 left join (select ebook_id,view_count,vote_count from ebook_snapshot
+where ebook_snapshot.date=date_sub(curdate(),interval 1 day )) t2
+    on t1.ebook_id=t2.ebook_id
+set t1.view_increase=(t1.view_count-ifnull(t2.view_count,0)),
+    t1.vote_increase=(t1.vote_count-ifnull(t2.vote_count,0))
+where t1.date=curdate();
