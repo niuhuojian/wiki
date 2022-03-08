541d11422ed95f1fadf2f41aea5f672dde116b09
jake <1727546144@qq.com>
2022-03-08 15:14:41 +0800

    对数据库进行加密

--- wiki/src/test/java/demo/WikiApplicationTests.java
+++ wiki/src/test/java/demo/WikiApplicationTests.java
@@ -1,13 +1,26 @@
 package demo;
 
+import org.jasypt.encryption.StringEncryptor;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WikiApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
+    @Autowired
+    private StringEncryptor stringEncryptor;
 
     @Test
     void contextLoads() {
+        Environment environment = applicationContext.getBean(Environment.class);
+        String mysql = environment.getProperty("spring.datasource.password");
+        System.out.println(stringEncryptor.encrypt(mysql));
+        System.out.println(stringEncryptor.encrypt("Rootroot1!"));
     }
 
 }
